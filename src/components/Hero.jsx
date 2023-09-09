ffd9067aa7c08b89cc3fe08a9be43678adbc4c25
Sakthi <sakthim61797@gmail.com>
2023-09-09 23:01:39 +0530

    init

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,7 +20,7 @@ const Hero = () => {
           <div  className=" w-1 h-[350px] violet-gradient rounded-tr-full  rounded-tl-full "></div>
         </div>
 
-        <div className="px-4 py-7 flex flex-col  jutstify-center items-center  gap-8 ">
+        <div className="px-4 py-7 flex flex-col  jutstify-center 2xl:items-center  gap-5 ">
           <p className="xl:text-xl " data-aos="fade-left" data-aos-duration="1000" >TCE welcomes you to </p>
           <h2 data-aos="fade-left" data-aos-duration="1100" className="text-4xl font-bold lg:text-5xl ">
             <span   className=" text-violet-500">TCE-MDU</span> Hackathon
@@ -34,7 +34,7 @@ const Hero = () => {
           </div>
 
           <div className=" 2xl: flex 2xl:gap-6 flex-col  gap-6 md:flex-row ">
-            <Link to='/register' ><button data-aos="fade-left" data-aos-duration="1300" className="box px-8 py-2  bg-violet-600 rounded-lg font-bold w-[270px]  xl:text-xl xl:py-3" >Register</button>
+            <Link to='/register' ><button data-aos="fade-left" data-aos-duration="1300" className="box px-8 py-2  bg-violet-600 rounded-lg font-bold w-[270px]  xl:text-xl xl:py-3 2xl:w-[320px]" >Register</button>
             </Link>
             <ScrollLink to="hackathon" smooth={true} duration={500} offset={-50} >   <button data-aos="fade-left" data-aos-duration="2000" className=" px-8 py-2 rounded-lg box1 font-bold w-[270px] xl:text-xl xl:py-3 2xl:w-[320px] md:w-[200px]" >Hackathon</button>
             </ScrollLink>
